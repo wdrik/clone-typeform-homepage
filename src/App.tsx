e36501955a389f02c7c7e98a28f64436377bb072
Iorgen Wildrik <w.drik@outlook.com>
2020-09-21 20:16:02 -0300

    feat: add Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global';
+import { Main } from './styles';
+
+
 function App() {
   return (
-    <div className="App">
-      Rocketseat
-    </div>
+    <>
+      <GlobalStyles />
+
+      <Main style={{ height: '1610vh' }}>
+        kkkk
+      </Main>
+    </>
   );
 }
 
